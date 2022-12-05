b8b45d53481d63650603fa0f56a21d90641e687e
Wisnu1110 <106785914+Wisnu1110@users.noreply.github.com>
2022-12-05 15:32:38 +0700

    config.js

--- options/config.js
+++ options/config.js
@@ -21,9 +21,9 @@ NO 2 : 085157740529
 const fs = require('fs')
 const chalk = require('chalk')
 
-global.owner = ["6281548926840"]
-global.ownerName = 'Zero YT7'
-global.botName = 'Popeye-MD'
+global.owner = ["6281393936306"]
+global.ownerName = 'Wisnu'
+global.botName = 'DoroBot-MD'
 global.prefa = ['','!','.','🐦','🐤','🗿','/']
 global.sp = '⭔'
 
